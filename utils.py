c2192a5dac71674397ddb01448315d6f2916a3fb
Yuhui <yuhuicaowork@gmail.com>
2026-03-20 22:21:29 +1300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 
 import base64
 import requests
-import pandas as pd
+import streamlit as st
 
 def save_csv(df, username):
     filename = f"{username}_top50_tracks.csv"
     df.to_csv(filename, index=False)
     return filename
 
+@st.cache_data(ttl=3600)
 def img_to_base64(url):
     try:
         response = requests.get(url, timeout=5)
